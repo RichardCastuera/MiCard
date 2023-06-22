1683bbc72ec4cdee1a063ab3cc719f56f9534679
unknown <richardcastuera16@gmail.com>
2023-06-22 16:22:47 +0800

    task(mi_card_app): Added a content inside the container with margin and padding

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -12,8 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
