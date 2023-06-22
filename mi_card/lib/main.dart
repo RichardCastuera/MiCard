9125ba4f1c3d10188a4b674bae739cd7371afb91
unknown <richardcastuera16@gmail.com>
2023-06-22 16:09:08 +0800

    task(mi_card_app): Added a Statelesswidget

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.red,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
